734c3a636857f22d84414bebb2032ae6c7a8f18c
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2017-09-28 16:11:51 +0200

    checking whether the user is permitted in Higher Order Component

--- src/components/accessControl.hoc.js
+++ src/components/accessControl.hoc.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-// import _ from 'lodash';
+import _ from 'lodash';
 
-function accessControl(AccessedComponent, userPermissions, neededPermissions, onFail) {
+// @TODO comment this shit
+function accessControl(
+  AccessedComponent,
+  userPermissions,
+  neededPermissions,
+) {
   return class AccessControl extends Component {
     constructor(props) {
       super(props);
+
+      if (!_.isEmpty(userPermissions)
+        && !this.checkPermissions()
+      ) {
+        console.log('user is not permitted');
+      }
     }
 
-    componentWillMount() {
-      // const permissionsIntersection = _.intersection(userPermissions, neededPermissions);
-
-      // console.log(permissionsIntersection);
-      console.log('hoc');
+    checkPermissions() {
+      return _.intersection(userPermissions, neededPermissions).length;
     }
+
     render() {
       return <AccessedComponent {...this.props}/>;
     }
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
+import PropTypes from 'prop-types';
+import { createBrowserHistory } from 'history';
 
 import App from 'containers/App';
 import Admin from 'pages/Admin';
@@ -11,11 +12,33 @@ import NotFoundPage from 'pages/NotFoundPage.js';
 import AccessControl from './components/accessControl.hoc';
 
 class Routes extends Component {
+  static propTypes = {
+    auth: PropTypes.shape({
+      user: PropTypes.shape({
+        username: PropTypes.string,
+        permissions: PropTypes.arrayOf(PropTypes.string),
+      }),
+    }),
+  };
+
+  constructor(props) {
+    super(props);
+    this.history = createBrowserHistory();
+  }
   render() {
-    const accessibleAdmin = AccessControl(Admin, null, ['ACCESS_ADMIN']);
+    let permissions = [];
+
+    const { auth } = this.props;
+    const { user } = auth;
+
+    if (typeof user !== 'undefined') {
+      permissions = user.permissions;
+    }
+
+    const accessibleAdmin = AccessControl(Admin, permissions, ['ACCESS_ADMIN']);
 
     return (
-      <ConnectedRouter history={createHistory()}>
+      <ConnectedRouter history={this.history}>
         <App>
           <Switch>
             <Route exact path="/" component={HomePage} />
