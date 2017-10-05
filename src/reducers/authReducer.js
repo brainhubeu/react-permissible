12bd6a0f7bdddcbfd0b5b916f08de6b8fc6a1623
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2017-10-05 15:23:13 +0200

    CR fixes

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -11,40 +11,46 @@ import initialState from './initialState';
 export default function auth(state = initialState.auth, action) {
   switch (action.type) {
     case FETCH_USERS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetching: true,
         fetchingError: null,
-      });
+      };
     case FETCH_USERS_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetching: false,
         fetchingError: null,
         users: action.result.users,
-      });
+      };
     case FETCH_USERS_FAILURE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         fetching: false,
         fetchingError: null,
         users: [],
-      });
+      };
     case LOGIN_ACTION:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         loggingIn: true,
         loginError: null,
-      });
+      };
     case LOGIN_ACTION_SUCCESS:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         loggingIn: false,
         user: action.result.user,
         isAuthenticated: true,
-      });
+      };
     case LOGIN_ACTION_FAILURE:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         loggingIn: false,
         loginError: action.error,
         user: null,
         isAuthenticated: false,
-      });
+      };
     default:
       return {
         ...state,
--- src/reducers/viewReducer.js
+++ src/reducers/viewReducer.js
@@ -7,17 +7,19 @@ import initialState from './initialState';
 export default function view(state = initialState.view, action) {
   switch (action.type) {
     case SHOW_MODAL:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         modalVisible: true,
         modalTitle: action.result.modalTitle,
         modalMessage: action.result.modalMessage,
-      });
+      };
     case HIDE_MODAL:
-      return Object.assign({}, state, {
+      return {
+        ...state,
         modalVisible: false,
         modalTitle: '',
         modalMessage: '',
-      });
+      };
     default:
       return state;
   }
